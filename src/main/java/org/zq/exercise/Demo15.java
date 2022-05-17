b62006fe7c7ae945c260e29515f77bcd3c458df1
zq <1360558144@qq.com>
2022-05-17 21:45:39 +0800

    msg

---
+++ src/main/java/org/zq/exercise/Demo15.java
@@ -0,0 +1,24 @@
+package org.zq.exercise;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * @description:三数之和
+ * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0
+ * @author:zq
+ * @time:7:38
+ */
+
+public class Demo15 {
+    public static void main(String[] args) {
+
+    }
+    public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        return null;
+    }
+}
